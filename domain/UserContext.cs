a76e726058aa156d7c032ba9eae14c4a27720fad
0298287677@mail.ru <0298287677@mail.ru>
2023-01-31 14:51:38 +0300

    Impemented NET6 with Authentification scheme

---
+++ domain/UserContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Models
+{
+    internal class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
